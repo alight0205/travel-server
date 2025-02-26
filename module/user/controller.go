9725578ed8d74a883ccf7b016876912cd9c9f9b4
alight0205 <1778911090@qq.com>
2025-02-26 17:50:26 +0800

    feat: 查询用户列表

---
+++ module/user/controller.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"travel-server/global"
+	"travel-server/middleware"
+	"travel-server/model"
+	"travel-server/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter(r *gin.RouterGroup) {
+	g1 := r.Group("/admin/user", middleware.JWTAuth(), middleware.AdminAuth())
+
+	g1.GET("/query_list", utils.WrapHandler(queryList, &QueryListReq{}))
+}
+
+// @Summary 用户列表
+// @Tags 用户管理
+// @Produce  application/json
+// @Param data query QueryListReq    true  "查询参数"
+// @Router /api/admin/user/query_list [get]
+// @Param Authorization header string true "Authorization"
+// @Produce json
+// @Success 200 {object} res.Response{}]
+func queryList(c *gin.Context, req QueryListReq) (data any, err error) {
+	var users []model.User
+	var total int64
+
+	query := global.DB.Model(&model.User{})
+	if req.ID != 0 {
+		query = query.Where("id = ?", req.ID)
+	}
+	if req.UserName != "" {
+		query = query.Where("username like ?", "%"+req.UserName+"%")
+	}
+	if req.Nickname != "" {
+		query = query.Where("nickname like ?", "%"+req.Nickname+"%")
+	}
+	if req.Role != 0 {
+		query = query.Where("role = ?", req.Role)
+	}
+
+	query.Count(&total)
+
+	if err = query.Order("created_at desc").Limit(req.PageSize).Offset((req.PageNum - 1) * req.PageSize).Find(&users).Error; err != nil {
+		return
+	}
+	data = map[string]any{
+		"list":  users,
+		"total": total,
+	}
+	return
+}
---
+++ module/user/dto.go
@@ -0,0 +1,11 @@
+package user
+
+import "travel-server/model"
+
+type QueryListReq struct {
+	model.PageInfo
+	ID       int    `form:"id" json:"id"`
+	UserName string `form:"name" json:"name"`
+	Nickname string `form:"nickname" json:"nickname"`
+	Role     int    `form:"role" json:"role"`
+}
